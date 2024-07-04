0ea0ad575f9a55c589ca0cda50ded872c8678983
KMK <gpvkxhekek@naver.com>
2024-07-04 17:25:51 +0900

    first commit

---
+++ src/main/java/com/example/athletetransfermarket/AthleteTransferMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.athletetransfermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteTransferMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleteTransferMarketApplication.class, args);
+	}
+
+}
